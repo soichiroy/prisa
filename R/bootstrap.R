ef6dd4ce87f11ed019952f872dd8673db90b6732
sou412 <sou412snow@gmail.com>
2025-05-11 17:06:53 -0400

    Move some functions to independent scripts

---
+++ R/bootstrap.R
@@ -0,0 +1,105 @@
+
+#' Run bootstrap
+#' 
+#' @importFrom parallel makeCluster stopCluster
+#' @importFrom doParallel registerDoParallel
+#' @importFrom foreach foreach %do% %dopar%
+#' @importFrom doRNG registerDoRNG
+#' @import dplyr
+#' @noRd 
+.RunBootstrap <- function(
+    main_model,
+    proxy_model,
+    data_list,
+    point_estimate,
+    options,
+    args_main_model,
+    args_proxy_model) {
+  
+  # Extract option values
+  n_boot <- options$n_boot
+  use_full <- options$use_full
+  use_parallel <- options$use_parallel
+  seed_value <- options$seed_value
+  n_cores <- options$n_cores
+
+  n_estimates_labeled <- point_estimate$n_estimates_labeled
+  n_estimates_full <- point_estimate$n_estimates_full
+
+  # Register parallel backend
+  if (use_parallel) {
+    cl <- parallel::makeCluster(n_cores)
+    doParallel::registerDoParallel(cl)
+    doRNG::registerDoRNG(seed_value)
+    on.exit({
+      parallel::stopCluster(cl)
+      foreach::registerDoSEQ()
+    })
+  } else {
+    # When use_parallel is FALSE, use sequential processing
+    foreach::registerDoSEQ()
+  }
+
+  # Bootstrap for the labeled data to estimate the variance-covariance matrix
+  # of the main estimators based on the labeled data.
+  boot_estimate_labeled <- foreach(
+    i = seq_len(n_boot),
+    .combine = rbind,
+    .packages = c("dplyr"),
+    .inorder = FALSE,
+    .export = c(".GetPointEstimatesLabeled")
+  ) %dopar% {
+      data_labeled_resampled <- 
+        slice_sample(data_list$dat_labeled, prop = 1, replace = TRUE)
+      .GetPointEstimatesLabeled(
+        main_model,
+        proxy_model,
+        data_labeled_resampled,
+        args_main_model,
+        args_proxy_model
+      )
+  }
+  vcov_labeled <- cov(boot_estimate_labeled)
+
+  # Exit the function if boot_full is FALSE (skip the bootstrap for the entire
+  # data).
+  if (isFALSE(use_full)) {
+    # Return vcov estimate
+    return(list(
+      vcov_labeled = vcov_labeled,
+      vcov_full = NULL,
+      vcov_main_diff = NULL
+    ))
+  }
+
+
+  # Estimate the variance covariance matrix of the biased estimator based on 
+  # the unlabeled data or the full data
+  data_main <- data_list$dat_full
+  
+  # Bootstrap for the full (or unlabeled) data
+  boot_estimate_full <- foreach(
+    i = seq_len(n_boot),
+    .combine = rbind,
+    .inorder = FALSE,
+    .packages = c("dplyr")
+  ) %dopar% {
+      data_main_resampled <- slice_sample(data_main, prop = 1, replace = TRUE)
+      do.call(proxy_model, c(list(data_main_resampled), args_proxy_model))
+  }
+
+  # VCOV(tau_proxy_ell - tau_proxy_full)
+  boot_estimate_diff <- boot_estimate_labeled[, -1] - boot_estimate_full
+  vcov_full <- cov(boot_estimate_diff)
+
+  # Cov(tau_main_ell, tau_proxy_ell - tau_proxy_full)
+  vcov_main_diff <- 
+    as.vector(cov(boot_estimate_labeled[, 1], boot_estimate_diff))
+
+  # Return bootstrapped variance-covariance estimates
+  list(
+    vcov_labeled = vcov_labeled,
+    vcov_full = vcov_full,
+    vcov_main_diff = vcov_main_diff
+  )
+}
---
+++ R/estimators.R
@@ -0,0 +1,147 @@
+
+.GetPointEstimates <- function(
+    main_model,
+    proxy_model,
+    data_list,
+    args_main_model,
+    args_proxy_model) {
+
+  # Unbiased estimator
+  tau_ell <-
+    do.call(main_model, c(list(data_list$dat_labeled), args_main_model))
+
+  if (length(as.vector(tau_ell)) > 1) {
+    stop("The main_model function must return a scalar value.")
+  }
+
+  # Biased estimators
+  delta_ell <- do.call(
+    proxy_model, c(list(data_list$dat_labeled), args_proxy_model)
+  )
+  delta_full <- do.call(
+    proxy_model, c(list(data_list$dat_full), args_proxy_model)
+  )
+
+  # Create control variate estimators
+  delta_diff <- as.vector(delta_ell) - as.vector(delta_full)
+  
+  # Return estimates: Assume the first element is the main model estimate
+  estimates <- c(tau_ell, delta_diff)
+  n_estimates_labeled <- length(estimates)
+  n_estimates_full <- length(delta_diff)
+  list(
+    estimates = estimates, 
+    n_estimates_labeled = n_estimates_labeled, 
+    n_estimates_full = n_estimates_full
+  )
+}
+
+.GetPointEstimatesLabeled <- function(
+    main_model,
+    proxy_model,
+    data_labeled_resampled,
+    args_main_model,
+    args_proxy_model
+) {
+
+  # Unbiased estimator
+  tau_ell <- do.call(
+    main_model, c(list(data_labeled_resampled), args_main_model)
+  )
+
+  # Biased estimators
+  delta_ell <- do.call(
+    proxy_model, c(list(data_labeled_resampled), args_proxy_model)
+  )
+
+  # Return estimates
+  estimates <- c(tau_ell, delta_ell) 
+  estimates
+}
+
+
+#' Estimate optimal coefficients
+#' 
+#' @param cov_estimates A list of bootstrap variance-covariance estimates.
+#' @noRd
+.EstimateOptimalCoefficients <- function(cov_estimates, prop, n_ell) {
+  vcov_labeled <- cov_estimates$vcov_labeled
+  # Covariance between the main and proxy model estimates
+  #  * [1,1] element is the variance of the main unbiased estimator 
+  cov_main_proxy <- as.vector(vcov_labeled[1, -1])
+  
+  # Variance covariance matrix of the proxy estimator
+  vcov_full <- cov_estimates$vcov_full
+
+  if (!is.null(vcov_full)) {
+    # When the vcov_full is available, estimate the coefficient using the 
+    # following formula:
+    #   A* = Cov(tau_main_ell, tau_proxy_diff) / V(tau_proxy_diff)
+    coef_estimates <- as.vector(solve(vcov_full, cov_estimates$vcov_main_diff))
+    return(coef_estimates)
+  }
+
+  # When the vcov_full is not available, use the labeled set estimator to
+  # estimate the coefficients.
+  #  A* = COV(tau_proxy_ell, tau_main_ell) / V(tau_proxy_ell)
+  coef_estimates <- as.vector(solve(vcov_labeled[-1, -1], cov_main_proxy))
+  coef_estimates
+}
+
+.CombineEstimates <- function(point_estimate, coef_estimates) {
+  tau_ell <- point_estimate[1]
+  delta_diff <- point_estimate[-1]
+
+  # Proposed estimator: unbiased + coef * (cv_estimators)
+  combined_estimate <- tau_ell - as.vector(coef_estimates %*% delta_diff) 
+  combined_estimate
+}
+
+#' Estimate the variance of the proposed estimator
+#' 
+#' @noRd
+.EstimateVariance <- function(
+  cov_estimates, coef_estimates, prop, n_ell, options
+) {
+  # Variance covariance of the main and proxy estimators
+  vcov_labeled <- cov_estimates$vcov_labeled
+  cov_main_proxy <- as.vector(vcov_labeled[1, -1])
+
+  # Variance of the original estimator
+  var_tau_ell <- vcov_labeled[1, 1]
+
+  # vcov_main_diff is NULL when use_full is FALSE.
+  if (is.null(cov_estimates$vcov_main_diff)) {
+    # Variance of the proposed estimator:
+    #   Additional (1 - prop) scaling because cov_main_proxy is based on the
+    #   labeled data.
+    var_est <- var_tau_ell - 
+      (1 - prop) * as.vector(cov_main_proxy %*% coef_estimates) 
+  } else {
+    # Use the full formula to estimate the variance
+    var_est <- var_tau_ell - 
+      as.vector(cov_estimates$vcov_main_diff %*% coef_estimates)
+  }
+
+  # Check the variance estimate
+  var_theoretical_limit <- prop * var_tau_ell
+  if (var_est < var_theoretical_limit && !options$debug_mode) {
+    stop(
+      "Failed variance estimation. Please use use_full = TRUE option instead."
+    )
+  }
+   
+  if (var_est < var_theoretical_limit && options$debug_mode) {
+    warning(
+      "Estimated variance is too small.", 
+      "Using the theoretical limit instead.", 
+      "Only for debugging."
+    )
+    var_est <- var_theoretical_limit
+  }
+  
+
+  # Compute the variance reduction factor in terms of ELSS
+  elss <- n_ell * var_tau_ell / var_est
+  list(var = var_est, elss = elss)
+}
