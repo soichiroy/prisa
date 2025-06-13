a50b69f9f87a6437fbbcff747274c9af65c052d4
sou412 <sou412snow@gmail.com>
2025-06-13 11:00:10 -0400

    Add a wrapper for lm

---
+++ R/prisa-lm.R
@@ -0,0 +1,40 @@
+
+#' Implement the Prediction-error Robust Inference for linear regression
+#' 
+prisa_lm <- function(
+  main_formula,
+  proxy_formula,
+  data,
+  labeled_set_var_name,
+  options = SetOptions(
+    n_boot = 500,
+    use_full = TRUE,
+    use_parallel = TRUE,
+    n_cores = parallel::detectCores() - 1,
+    cluster_var_name = NULL
+  ),
+  args_main_model = list(),
+  args_proxy_model = list()
+) {
+
+  # TODO: Support for additional arguments in the main and proxy models
+  main_lm_model <- function(df) {
+    fit <- lm(as.formula(main_formula), data = df)
+    return(fit$coefficients)
+  }
+
+  proxy_lm_model <- function(df) {
+    fit <- lm(as.formula(proxy_formula), data = df)
+    return(fit$coefficients)
+  }
+
+  .PredictionErrorRobustInference(
+    main_model = main_lm_model,
+    proxy_model = proxy_lm_model,
+    data = data,
+    labeled_set_var_name = labeled_set_var_name,
+    options = options,
+    args_main_model = args_main_model,
+    args_proxy_model = args_proxy_model
+  )
+}
