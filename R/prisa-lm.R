3d6870147ba77265e2a2154e9f3c5aa9a01892e0
sou412 <sou412snow@gmail.com>
2025-06-22 16:38:14 -0400

    Combine the implementation of prisa_lm and prisa_glm

--- R/prisa-lm.R
+++ R/prisa-lm.R
@@ -1,8 +1,9 @@
-#' Implement the Prediction-error Robust Inference for linear regression
+#' Prediction-error Robust Inference for linear regression
 #' 
 #' @param main_formula A formula for the main model. The same argument as in `lm()`.
 #' @param proxy_formula A formula for the proxy model. The same argument as in `lm()`.
 #' @inheritParams prisa
+#' @seealso [stats::lm()]
 #' @export
 prisa_lm <- function(
   main_formula,
@@ -19,54 +20,25 @@ prisa_lm <- function(
   args_main_model = list(),
   args_proxy_model = list()
 ) {
-  if (!is.list(args_main_model)) {
-    stop("args_main_model must be a list.")
-  }
-  if (!is.list(args_proxy_model)) {
-    stop("args_proxy_model must be a list.")
-  }
-
-  # TODO: Support for additional arguments in the main and proxy models
-  # TODO: Check the scope of main_formula and proxy_formula objects
-  .main_lm_model <- function(df, formula, weights = NULL) {
-    fit <- lm(formula, data = df, weights = weights)
-    return(fit$coefficients)
-  }
-
-  .proxy_lm_model <- function(df, formula, weights = NULL) {
-    fit <- lm(formula, data = df, weights = weights)
-    return(fit$coefficients)
-  }
-
-  # Explicitly pass formula objects
-  args_main_model$formula <- as.formula(main_formula)
-  args_proxy_model$formula <- as.formula(proxy_formula)
-
-  # TODO: When weights are provided, update ELSS to reflect the weights for the 
-  # labeled only estimator.
-  if ("weights" %in% names(args_main_model)) {
-    args_main_model$weights <- NULL
-  }
-  if ("weights" %in% names(args_proxy_model)) {
-    args_proxy_model$weights <- NULL
-  }
-
-  .PredictionErrorRobustInference(
-    main_model = .main_lm_model,
-    proxy_model = .proxy_lm_model,
+  .CallPrisaGlm(
+    main_formula = main_formula,
+    proxy_formula = proxy_formula,
     data = data,
     labeled_set_var_name = labeled_set_var_name,
+    family = gaussian(),
     options = options,
     args_main_model = args_main_model,
     args_proxy_model = args_proxy_model
   )
 }
 
-#' Implement the Prediction-error Robust Inference for generalized linear models
+#' Prediction-error Robust Inference for generalized linear models
 #' 
 #' @param main_formula A formula for the main model. The same argument as in `glm()`.
 #' @param proxy_formula A formula for the proxy model. The same argument as in `glm()`.
-#' @param family A description of the error distribution and link function to be used in the model. See `glm()`.
+#' @param family A description of the error distribution and link function to be
+#'   used in the model. See the family argument in the `glm()` function for the 
+#'   details.
 #' @inheritParams prisa
 #' @seealso [stats::glm()]
 #' @export
@@ -75,7 +47,7 @@ prisa_glm <- function(
   proxy_formula,
   data,
   labeled_set_var_name,
-  family = gaussian(),
+  family,
   options = SetOptions(
     n_boot = 500,
     use_full = TRUE,
@@ -83,6 +55,29 @@ prisa_glm <- function(
     n_cores = parallel::detectCores() - 1,
     cluster_var_name = NULL
   ),
+  args_main_model = list(),
+  args_proxy_model = list()
+) {
+  .CallPrisaGlm(
+    main_formula = main_formula,
+    proxy_formula = proxy_formula,
+    data = data,
+    labeled_set_var_name = labeled_set_var_name,
+    family = family,
+    options = options,
+    args_main_model = args_main_model,
+    args_proxy_model = args_proxy_model
+  )
+}
+
+# Internal implementation for both prisa_lm and prisa_glm
+.CallPrisaGlm <- function(
+  main_formula,
+  proxy_formula,
+  data,
+  labeled_set_var_name,
+  family,
+  options,
   args_main_model = list(),
   args_proxy_model = list()
 ) {
